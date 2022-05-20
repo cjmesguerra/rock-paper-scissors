a1e0ccb85a08c94042906115508d029c3e7dae70
Chris <christerjohnesguerra@gmail.com>
2022-05-20 19:33:37 +0800

    Upload initial playable state

---
+++ script.js
@@ -0,0 +1,84 @@
+function playerPlay() {
+    let player_choice = prompt("Rock, Paper or Scissors?");
+    return player_choice;
+}
+
+function computerPlay() {
+    let rsp_arr = ["Rock", "Paper", "Scissors"];
+
+    let rsp_choice = rsp_arr[Math.floor(Math.random() * rsp_arr.length)];
+
+    return rsp_choice;
+}
+
+function playRound (playerSelection, computerSelection) {
+    
+    player = playerSelection.toLowerCase();
+    computer = computerSelection.toLowerCase();
+    
+    console.log("Player: " + player);
+    console.log("Computer: " + computer);
+
+    if (player == computer) {
+        console.log("It's a Tie!");
+    } else if (player == "rock") {
+       if (computer == "paper") {
+            console.log("You Lost! Paper beats Rock");
+            return computer;
+       } else {
+            console.log("You Won! Rock beats Scissors");
+            return player;
+       }
+    } else if (player == "scissors") {
+        if (computer == "rock") {
+            console.log("You Lost! Rock beats Scissors");
+            return computer;
+        } else {
+            console.log("You Won! Scissors beats Paper");
+            return player;
+        }
+     } else if (player == "paper") {
+        if (computer == "Scissors") {
+            console.log("You Lost! Scissors beats Paper");
+            return computer;
+        } else {
+            console.log("You Won! Paper beats Rock");
+            return player;
+        }
+     } 
+    
+}
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = playerPlay();
+        const computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+        if (result === player) {
+            playerScore++;
+           
+        } else if (result === computer) {
+            computerScore++;
+        }
+     }
+
+     
+     if (playerScore > computerScore) {
+         result = "Congratulations! Player Won the Match! Points: " + playerScore;
+         return result;
+     } else if (playerScore < computerScore) {
+         result = "Sorry! Computer Won the Match! Points: " + computerScore;
+         return result;
+     } else if (playerScore == computerScore) {
+         result = "It's a Tie!"
+         return result;
+     }
+} 
+
+console.log(game());
+
+/* console.log("Player: " + player);
+    console.log("Computer: " + computer); */
